c2d9c47f5b70b384afffce4737b194ec3017db82
jairomelo <jairom@colmich.edu.mx>
2023-01-26 08:55:29 -0600

    dir ejemplo

--- __gestion/gestion.py
+++ __gestion/gestion.py
@@ -6,8 +6,10 @@ excel = pd.read_excel(os.path.join('__conf/lista_clase.xlsx'), sheet_name='Resum
 estudiantes = excel.loc[14:24, ['Información de curso']]
 
 os.makedirs('estudiantes', exist_ok=True)
+os.mkdir(os.path.join('estudiantes', 'Ejemplo'))
+with open(os.path.join('estudiantes', 'Ejemplo', 'README.md'), 'w') as readme:
+    readme.write('# Ejemplo')
 
-# for each estudiante make a new folder in the folder 'estudiantes'
 for estudiante in estudiantes['Información de curso']:
     estudiante = estudiante.title()
     estudiante = estudiante.split(',')
@@ -19,4 +21,4 @@ for estudiante in estudiantes['Información de curso']:
         with open(os.path.join('estudiantes', estudiante, 'README.md'), 'w') as readme:
             readme.write(f'# {estudiante}')
     except FileExistsError:
-        print('Folder already exists')
+        print('El directorio ya fue creado')
