a69a25f53f07eb4f7ce86f828ab3fbe03f815dba
Christian Reinach <cd.reinach@uniandes.edu.co>
2023-02-05 19:54:25 -0500

    "Entrega de la actividad 2"

---
+++ estudiantes/Christian David Reinach Baumgartner/Actividad 2/act2_4.py
@@ -0,0 +1,12 @@
+#Ejercicio 4
+
+frase_1= "A la larga, el perro acabará comprometiéndose con el gato."
+frase_2= "A la larga, la serpiente acabará comprometiéndose con el ratón."
+
+
+print(f' Las frases concatenadas son "{frase_1+" "+"Y"+" "+frase_2.lower()}" \
+    \n La frase 1 repetida tres veces es "{frase_1*3}" \
+    \n La palabra "János" está en la frase 1; esto es {"János" in frase_1}. \
+    \n La palabra "David" no está en la frase 2; esto es {"David" not in frase_2}. \
+    \n La frase 1 es la misma que la frase 2; esto es {frase_1 == frase_2}. \
+    \n La frase 1 no es igual a la frase 2; esto es {frase_1 != frase_2}.')
