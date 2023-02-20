3a545267e0e0848df1a1fc3127dc219ddcf78715
pipen77 <pipen77@hotmail.com>
2023-02-20 11:36:33 -0500

    Archivos Ejercicios Actividad 4

---
+++ estudiantes/Julian Felipe Benavides Nieves/Act4/Ejercicio2_Act4_JulianBenavides.py
@@ -0,0 +1,15 @@
+# Catalogo
+
+libros = ['Cuentos completos 1; Isaac Asimov; 2009; Ficción', 'Cuentos completos 2; Isaac Asimov; 2009; Ficción', 'De Animales a Dioses; Yuval Noah Harari; 2015; Historia General', 'Homo Deus; Yuval Noah Harari; 2016; Historia General', 'Prohibido Nacer; Trevor Noah; 2017; Autobiografía']
+
+print(libros)
+print(len(libros))
+
+nuevos_libros = ["Somos Luces Abismales; Carolina Sanín; 2018; Ensayos Literarios", "Muerte con Pingüino; Andrei Kurkov; 1996; Novela", "Que Viva la Música!; Andrés Caicedo; 1977; Novela"]
+
+libros.extend(nuevos_libros)
+
+print(libros)
+print(len(libros))
+
+
