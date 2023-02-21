ab507b2a81d4afc52029bbf8c3b3cc78aaf52fe2
cogueur <c.guevarau@uniandes.edu.co>
2023-02-21 12:43:00 -0500

    Entrega de la actividad 2 - Conni GU

---
+++ estudiantes/Conni Guevara Urrego/Actividad 2/act2_2.py
@@ -0,0 +1,31 @@
+#Ejercicio 2 Variables
+
+#Variables Personajes
+nombre1 = "Gabriel"
+print(nombre1)
+nombre2 = "Carla" 
+print(nombre2)
+
+#Variables Edad
+edad1 = 15
+print(edad1)
+edad2 = 18
+print(edad2)
+
+#Otras variables
+lugar = Valledupar 
+print(lugar)
+tiempo = "2 horas"
+print(tiempo)
+
+
+#Conversación
+frase1 = f"Hola, mi nombre es {nombre1} y tengo {edad1} años."
+print(frase1)
+frase2 = f"Hola, mi nombre es {nombre2} y tengo {edad} años."
+print(frase2)
+frase3 = f"Hemos estado conversando por {tiempo} mientras estábamos en {lugar}"
+print(frase3)
+frase4 = f"Entonces yo soy {edad2 - int(edad1)} años mayor."
+print(frase4)
+
