3bc1417d042dcb0319e5f2f602e0b24809bc355b
cogueur <c.guevarau@uniandes.edu.co>
2023-03-07 12:57:21 -0500

    Act2 y notas

--- estudiantes/Conni Guevara Urrego/Actividad 2/act2_2.py
+++ estudiantes/Conni Guevara Urrego/Actividad 2/act2_2.py
@@ -1,31 +1,37 @@
-#Ejercicio 2 Variables
-
-#Variables Personajes
 nombre1 = "Gabriel"
 print(nombre1)
-nombre2 = "Carla" 
-print(nombre2)
 
-#Variables Edad
 edad1 = 15
 print(edad1)
+
+
+nombre2 = "Carla"
+print(nombre2)
+
 edad2 = 18
 print(edad2)
 
-#Otras variables
-lugar = Valledupar 
+
+lugar = "Valledupar" 
 print(lugar)
+
 tiempo = "2 horas"
 print(tiempo)
 
-
-#Conversación
 frase1 = f"Hola, mi nombre es {nombre1} y tengo {edad1} años."
 print(frase1)
-frase2 = f"Hola, mi nombre es {nombre2} y tengo {edad} años."
+
+frase2 = f"Hola, mi nombre es {nombre2} y tengo {edad2} años."
 print(frase2)
+
 frase3 = f"Hemos estado conversando por {tiempo} mientras estábamos en {lugar}"
 print(frase3)
+
 frase4 = f"Entonces yo soy {edad2 - int(edad1)} años mayor."
 print(frase4)
+
+frases_completas = f" {frase1} + {frase2} + {frase3} + {frase4}"
+
+
+
 
---
+++ estudiantes/Conni Guevara Urrego/Actividad 2/act2_3.py
@@ -0,0 +1,42 @@
+##lower(): Convierte todas las letras de la frase a minúsculas
+
+oracion1 = "Seguiremos cantando. Un canto a mi tierra, un canto de resistencia"
+print(oracion1.lower())
+
+##upper(): Convierte todas las letras de la frase a mayúsculas
+
+oracion1 = "Seguiremos cantando. Un canto a mi tierra, un canto de resistencia"
+print(oracion1.upper())
+
+##capitalize(): Convierte la primera letra de la frase a mayúscula
+
+oracion1 = "Seguiremos cantando. Un canto a mi tierra, un canto de resistencia"
+print(oracion1.capitalize())
+
+##title(): Convierte la primera letra de cada palabra de la frase a mayúscula
+
+oracion1 = "Seguiremos cantando. Un canto a mi tierra, un canto de resistencia"
+print(oracion1.title())
+
+##swapcase(): Convierte todas las letras de la frase a mayúsculas y minúsculas
+
+oracion1 = "    Seguiremos cantando. Un canto a mi tierra, un canto de resistencia   "
+print(oracion1.swapcase())
+
+##strip(): Elimina los espacios en blanco al inicio y al final de la frase
+
+oracion1 = "    Seguiremos cantando. Un canto a mi tierra, un canto de resistencia   "
+print(oracion1.strip())
+
+##replace(): Reemplaza una palabra por otra en la frase
+
+oracion1 = "Seguiremos cantando. Un canto a mi tierra, un canto de resistencia"
+palabra1 = "canto"
+palabra2 = "grito"
+print(f"oracion1.replace{palabra1}")
+print(f"import{palabra2}")
+
+##split(): Divide la frase en palabras
+
+oracion1 = "Seguiremos cantando. Un canto a mi tierra, un canto de resistencia"
+print(oracion1.split())
