d62924721770a3f7fe6b89e8c49027085e47a952
jonathandhurtadoc <jd.hurtado@uniandes.edu.co>
2023-03-23 14:55:50 -0500

    entrega

--- estudiantes/Jonathan David Hurtado Cruz/act7/act7.1.py
+++ estudiantes/Jonathan David Hurtado Cruz/act7/act7.1.py
@@ -1,6 +1,9 @@
 bibliografia = {'000': {'tipo': '', 'autor': [{'nombre': '', 'apellido': ''}], 'titulo': '', 'editorial': '', 'fecha': 0, 'lugar': ''}, '001': {'tipo': 'novela', 'autor': [{'nombre': 'Fiodor', 'apellido': 'Dostoyenvski'}], 'titulo': 'El jugador', 'editorial': 'Alianza editorial', 'fecha': '1886', 'lugar': 'Rusia'}, '002': {'tipo': 'novela', 'autor': [{'nombre': 'Simone', 'apellido': 'deBeauvoir'}], 'titulo': 'Sangre de los otros', 'editorial': 'Seix Barral', 'fecha': '1944', 'lugar': 'Francia'}, '003': {'tipo': 'novela', 'autor': [{'nombre': 'Albert', 'apellido': 'Camus'}], 'titulo': 'El extrajenro', 'editorial': 'Éditions Gallimard', 'fecha': '1942', 'lugar': 'Francia'}, '004': {'tipo': 'novela', 'autor': [{'nombre': 'Jean-Paul', 'apellido': 'Sartre'}], 'titulo': 'La Naúsea', 'editorial': 'Éditions Gallimard', 'fecha': '1938', 'lugar': 'Francia'}, 
 '005': {'tipo': 'novela', 'autor': [{'nombre': 'Franz', 'apellido': 'Kafka'}], 'titulo': 'La Metaformosis', 'editorial': 'Kurt Wolff Verlag', 'fecha': '1915', 'lugar': 'Alemania'}, '006': {'fecha': '24.02.2023', 'periodico': [{'nombre': 'Noticias', 'apellido': 'DW'}], 'titulo': 'Los Grammy Latinos se entregarán en España en 2023', 'seccion': 'cultura', 'url': 'https://p.dw.com/p/4Nxh4'}, '007': {'revista': 'Journal of Open Humanities Data', 'autor': [{'nombre': 'Mei-Shin', 'apellido': 'Wu'}], 'titulo': 'Computer-Assisted Language Comparison: State of the Art', 'volumen': '6', 'fecha': '2020', 'numero': '2', 'url': 'https://doi.org/10.5334/johd.12 '}, '008': {'canal': [{'nombre': 'Darin', 'apellido': 'McNabb'}], 'titulo': 'El existencialismo: una introducción 1/2', 'fecha': '2013', 'url': 'https://www.youtube.com/watch?v=JotchOUEdfk'}}
 
+
+#ejercicio 1
+
 def buscar():
     """buscador por título a través de carácteres"""
      
@@ -42,3 +45,5 @@ if len(resultado_diferente) > 0:
                 print(f'- {lib1}')
 elif len(resultado_completo) == 0:
                 print(f'No se encontraron elementos que incluyan la palabra "{palabra}".')
+
+#ejercicio 2
