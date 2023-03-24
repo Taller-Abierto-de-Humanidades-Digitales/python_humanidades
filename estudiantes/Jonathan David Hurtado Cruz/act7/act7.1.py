329084a1f1e1ed1009df1a74fcc31ab91cbc9dba
jonathandhurtadoc <jd.hurtado@uniandes.edu.co>
2023-03-24 15:09:29 -0500

    avances parciales

--- estudiantes/Jonathan David Hurtado Cruz/act7/act7.1.py
+++ estudiantes/Jonathan David Hurtado Cruz/act7/act7.1.py
@@ -1,6 +1,5 @@
-bibliografia = {'000': {'tipo': '', 'autor': [{'nombre': '', 'apellido': ''}], 'titulo': '', 'editorial': '', 'fecha': 0, 'lugar': ''}, '001': {'tipo': 'novela', 'autor': [{'nombre': 'Fiodor', 'apellido': 'Dostoyenvski'}], 'titulo': 'El jugador', 'editorial': 'Alianza editorial', 'fecha': '1886', 'lugar': 'Rusia'}, '002': {'tipo': 'novela', 'autor': [{'nombre': 'Simone', 'apellido': 'deBeauvoir'}], 'titulo': 'Sangre de los otros', 'editorial': 'Seix Barral', 'fecha': '1944', 'lugar': 'Francia'}, '003': {'tipo': 'novela', 'autor': [{'nombre': 'Albert', 'apellido': 'Camus'}], 'titulo': 'El extrajenro', 'editorial': 'Éditions Gallimard', 'fecha': '1942', 'lugar': 'Francia'}, '004': {'tipo': 'novela', 'autor': [{'nombre': 'Jean-Paul', 'apellido': 'Sartre'}], 'titulo': 'La Naúsea', 'editorial': 'Éditions Gallimard', 'fecha': '1938', 'lugar': 'Francia'}, 
-'005': {'tipo': 'novela', 'autor': [{'nombre': 'Franz', 'apellido': 'Kafka'}], 'titulo': 'La Metaformosis', 'editorial': 'Kurt Wolff Verlag', 'fecha': '1915', 'lugar': 'Alemania'}, '006': {'fecha': '24.02.2023', 'periodico': [{'nombre': 'Noticias', 'apellido': 'DW'}], 'titulo': 'Los Grammy Latinos se entregarán en España en 2023', 'seccion': 'cultura', 'url': 'https://p.dw.com/p/4Nxh4'}, '007': {'revista': 'Journal of Open Humanities Data', 'autor': [{'nombre': 'Mei-Shin', 'apellido': 'Wu'}], 'titulo': 'Computer-Assisted Language Comparison: State of the Art', 'volumen': '6', 'fecha': '2020', 'numero': '2', 'url': 'https://doi.org/10.5334/johd.12 '}, '008': {'canal': [{'nombre': 'Darin', 'apellido': 'McNabb'}], 'titulo': 'El existencialismo: una introducción 1/2', 'fecha': '2013', 'url': 'https://www.youtube.com/watch?v=JotchOUEdfk'}}
-
+bibliografia = {'000': {'tipo': '', 'autor': [{'nombre': '', 'apellido': ''}], 'titulo': '', 'editorial': '', 'fecha': 0, 'lugar': ''}, '001': {'tipo': 'novela', 'autor': [{'nombre': 'Fiodor', 'apellido': 'Dostoyenvski'}], 'titulo': 'El jugador', 'editorial': 'Alianza editorial', 'fecha': '1886', 'lugar': 'Rusia'}, '002': {'tipo': 'novela', 'autor': [{'nombre': 'Simone', 'apellido': 'de-Beauvoir'}], 'titulo': 'Sangre de los otros', 'editorial': 'Seix Barral', 'fecha': '1944', 'lugar': 'Francia'}, '003': {'tipo': 'novela', 'autor': [{'nombre': 'Albert', 'apellido': 'Camus'}], 'titulo': 'El extrajenro', 'editorial': 'Éditions Gallimard', 'fecha': '1942', 'lugar': 'Francia'}, '004': {'tipo': 'novela', 'autor': [{'nombre': 'Jean-Paul', 'apellido': 'Sartre'}], 'titulo': 'La Naúsea', 'editorial': 'Éditions Gallimard', 'fecha': '1938', 'lugar': 'Francia'}, '005': {'tipo': 'novela', 'autor': [{'nombre': 'Franz', 'apellido': 'Kafka'}], 'titulo': 'La Metaformosis', 'editorial': 'Kurt Wolff Verlag', 'fecha': '1915', 'lugar': 'Alemania'}, '006': {'fecha': '25.02.2023', 'periodico': [{'nombre': 'Noticias', 'apellido': 'DW'}], 'titulo': 'Documental francés gana el Oso de Oro en la Berlinale', 'seccion': 'cultura', 'url': 'https://p.dw.com/p/4NzKy', 'tipo': 'periódico'}, '007': 
+{'fecha': '24.02.2023', 'periodico': [{'nombre': 'Noticias', 'apellido': 'DW'}], 'titulo': 'Los Grammy Latinos se entregarán en España en 2023', 'seccion': 'cultura', 'url': 'https://p.dw.com/p/4Nxh4', 'tipo': 'periódico'}, '008': {'revista': 'Journal of Open Humanities Data', 'autor': [{'nombre': 'Vincent', 'apellido': 'Ducatteeuw'}], 'titulo': 'Critical Reflections on Cinema Belgica: The Database for New Cinema History in Belgium', 'volumen': '9', 'fecha': '2023', 'numero': '1', 'url': 'https://doi.org/10.5334/johd.91', 'tipo': 'Articulo academico'}, '009': {'revista': 'Journal of Open Humanities Data', 'autor': [{'nombre': 'Li', 'apellido': 'Nguyen'}], 'titulo': 'Automatic Language Identification in Code-Switched Hindi-English Social Media Text', 'volumen': '7', 'fecha': '2021', 'numero': '7', 'url': 'https://doi.org/10.5334/johd.44 ', 'tipo': 'Articulo academico'}, '010': {'revista': 'Journal of Open Humanities Data', 'autor': [{'nombre': 'Mei-Shin', 'apellido': 'Wu'}], 'titulo': 'Computer-Assisted Language Comparison: State of the Art', 'volumen': '6', 'fecha': '2020', 'numero': '2', 'url': 'https://doi.org/10.5334/johd.12 ', 'tipo': 'Articulo académico'}, '011': {'canal': [{'nombre': 'Darin', 'apellido': 'McNabb'}], 'titulo': 'El existencialismo: una introducción 1/2', 'fecha': '2013', 'url': 'https://www.youtube.com/watch?v=JotchOUEdfk', 'tipo': 'video'}}
 
 #ejercicio 1
 
@@ -47,3 +46,32 @@ elif len(resultado_completo) == 0:
                 print(f'No se encontraron elementos que incluyan la palabra "{palabra}".')
 
 #ejercicio 2
+
+
+def buscador(titulos: str, año: int):
+        """"Esta función busca por año y titulo un libro del diccionario
+
+        parámetros
+        ----------------
+        titulos: str
+        tiene el título de los textos
+        
+        año: int
+        Año de publicaciones de los textos
+
+        """
+
+for lib in resultado_similar:
+            if lib not in resultado_completo:
+                resultado_diferente.append(lib)
+if len(resultado_completo) > 0:
+    print(f'para tu búsqueda, se encontraron {len(resultado_completo)} títulos que incluyen la palabra "el{palabra}":')
+for lib1 in resultado_completo:
+                print(f'- {lib1}')
+        
+if len(resultado_diferente) > 0:
+    print(f'para tu búsqueda se encontraron {len(resultado_diferente)} títulos que contienen los caracteres "{palabra}":')
+    for lib1 in resultado_diferente:
+                print(f'- {lib1}')
+elif len(resultado_completo) == 0:
+                print(f'No se encontraron elementos que incluyan la palabra "{palabra}".')
