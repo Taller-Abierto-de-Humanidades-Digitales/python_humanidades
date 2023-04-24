e7d609e74b71d7cd2a3c1098588bc99c35c911b4
jonathandhurtadoc <jd.hurtado@uniandes.edu.co>
2023-04-24 15:20:12 -0500

    otro intento de desempaquetar e interfaz

--- estudiantes/Jonathan David Hurtado Cruz/actividad_intermedia/interfaz.py
+++ estudiantes/Jonathan David Hurtado Cruz/actividad_intermedia/interfaz.py
@@ -5,16 +5,16 @@ from biblioteca import biblioteca
 def menu():
     print("1. Buscar por título")
     print("2. Buscar por autor")
-    print("3. Salir")
+    print("3. Buscar por tipo de texto")
+    print("4. Salir")
     opcion = input("Ingrese una opción: ")
     return opcion
 
 
 def main():
     opcion = menu()
-    while opcion != "3":
+    while opcion != "4":
         if opcion == "1":
-            #tipo = input("Ingrese el tipo de texto que desea buscar: ")
             titulo = input("Ingrese el título: ")
             biblio= biblioteca()
             buscatitulo("article-journal", titulo, biblio)
@@ -22,6 +22,14 @@ def main():
             autor = input("Ingrese el autor: ")
             tipo = input("Ingrese el tipo de texto: ")
             palabra = input("Ingrese la palabra clave para buscar: ")
+            try:
+                busqueda(palabra, autor, tipo) 
+                
+            except TypeError:
+                print("No se encontraron resultados")
+        elif opcion == "3":
+            tipo = input("Ingrese el tipo de texto que desea buscar: ")
+            biblio= biblioteca()
             try:
                 busqueda(palabra, autor, tipo) 
             except TypeError:
